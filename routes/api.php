d6379530f9d83f465f47c1dbde1453f8379abdfe
Lopez Perez Jose Manuel <jmanueldevelop@gmail.com>
2017-11-03 22:57:04 -0600

    API-REST v3
    se crearon las tablas en la BD y se agregaron las rutas que se ocuparan en el api.php

--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,24 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+//retorna el usuario si esta registrado
+Route::post('log',function(){
+	return response(['elemento1','elemento2'],200);
+});
+//ingresa la imagen
+Route::post('imagen',function(){
+	return response(['elemento3','elemento4'],200);
+});
+//obtiene la imagen
+Route::get('imagen/{id}',function(){
+	return response(['elemento5','elemento6'],200);
+});
+//ingresa el registro
+Route::post('registro',function(){
+	return response(['elemento7','elemento8'],200);
+});
+//obtiene el registro
+Route::get('registro/{id}',function(){
+	return response(['elemento9','elemento10'],200);
+});
