7fac7fdf666935d893b2774b2ca82276870c9f55
Lopez Perez Jose Manuel <jmanueldevelop@gmail.com>
2017-11-23 11:41:46 -0600

    API-REST vfinal
    funciona todo

--- app/Registro.php
+++ app/Registro.php
@@ -6,14 +6,19 @@ use Illuminate\Database\Eloquent\Model;
 
 class Registro extends Model
 {
+    //nombre de la base de datos como aparece en sqlite
     protected $table='Registros';
+    //elementos de la base da datos
     protected $fillable=array('kilometros','gasolina','kilos','login_id');
+    //elementos que no se devuelven
     protected $hidden=['created_at','updated_at'];
 
+    //metodo inverso de one to one
     public function user(){
     	return $this->belongsTo('App\User');
     }
 
+    //metodo de one to one 
     public function foto(){
     	return $this->hasOne('App\Foto');
     }
